ee1aae6d629bd56ff0d8e3769d6e7131f31e2bc9
Hanqi <t18016328120@gmail.com>
2026-01-20 21:21:36 +0100

    update game2

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,14 @@ mod game1;
 mod game2;
 
 use menu::draw_menu;
-use game1::Game;
-use game2::Game;
+use game1::Game as Game1;
+use game2::Game as Game2;
 
 #[derive(PartialEq)]
 enum GameState {
     Menu,
     Playing1,
+    Playing2,
 }
 
 #[macroquad::main("Whac-A-Mole")]
@@ -30,15 +31,15 @@ async fn main() {
     let hemlet_mole_image = load_image("images/helmet_mole.png")
         .await
         .expect("Failed to load helmet_mole image");
-    let hemlet_mole_texture = Arc::new(Texture2D::from_image(&hemlet_mole_image));
+    let _hemlet_mole_texture = Arc::new(Texture2D::from_image(&hemlet_mole_image));
 
     let cat_image = load_image("images/cat.png")
         .await
         .expect("Failed to load cat image");
-    let cat_texture = Arc::new(Texture2D::from_image(&cat_image));
+    let _cat_texture = Arc::new(Texture2D::from_image(&cat_image));
     
     let mut game_state = GameState::Menu;
-    let mut current_game: Option<Game> = None;
+    let mut current_game: Option<Game1> = None;
 
     loop {
         clear_background(LIGHTGRAY);
@@ -47,7 +48,7 @@ async fn main() {
             GameState::Menu => {
                 if let Some(difficulty) = draw_menu() {
                     if difficulty == 1 {
-                        current_game = Some(Game::new(difficulty, background_texture.clone(), mole_texture.clone()));
+                        current_game = Some(Game1::new(difficulty, background_texture.clone(), mole_texture.clone()));
                         game_state = GameState::Playing1;
                     }
                 }
